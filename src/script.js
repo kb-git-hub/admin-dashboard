181ee512af4d29b25bb890dfdb1c08c84db7a9d9
KB <kj.br@protonmail.com>
2022-09-09 23:54:44 -0500

    Fixed slider 100% positioning error

--- src/script.js
+++ src/script.js
@@ -1,24 +1,57 @@
-const inertialVelocitySlider = document.querySelector('#inertialVelocity')
-const inertialVelocityText = document.querySelector('#inertialVelocityDigits')
+const velocitySlider = document.querySelector('#inertialVelocity')
+const velocityText = document.querySelector('#inertialVelocityDigits')
 
 const altitudeSlider = document.querySelector('#altitude')
 const altitudeText = document.querySelector('#altitudeDigits')
 
 
 /*
-Gauges
+Time
 */
 let
-    gaugeStart = 0,
-    gaugeElapsed = 0,
-    allGaugeMeter = 0,
-    intervalAmount = 900,
-    intervalID
+    timeStart = 0,
+    timeElapsed = 0,
+    timeCurrent = 0,
+    timeIntervalID
 
-// Intertial gauges
+
+
+/*
+Velocity
+*/
 let
-    inertialVelocity = 0,
-    inertialVelocityDigits = 0
+    velocity = 0,
+    velocityMeter = 0,
+    velocityID
+
+
+function increaseVelocity() {
+    velocityID = setInterval(updateVelocity, 1000)
+
+    function updateVelocity() {
+        velocity = timeElapsed * 5
+        console.log('velocity:', velocity);
+        // velocityMeter = velocity/1000
+        // console.log('velocity meter:', velocityMeter)
+      
+
+        let variableVelocity = randomizeFlux(velocity, 1)
+        velocitySlider.style.width = variableVelocity + '%'
+        velocityText.textContent = `${capVelocityText(variableVelocity)} km/s`
+    }
+
+    function capVelocityText(velocity){
+        if (velocity < 10) return velocity.toFixed(1)
+        else return 10
+    }
+
+
+
+}
+
+
+
+
 
 function UpdateInertialVelocityDisplay(velocity) {
     let speed = (velocity / 10)
@@ -26,65 +59,88 @@ function UpdateInertialVelocityDisplay(velocity) {
     else return speed
 }
 
-//altitude gauges
-let
-    altitude = 0,
-    altitudeDigits = 0
 
-function UpdateAltitudeDisplay(allGaugeMeter) {
-    return ((allGaugeMeter / 1600)*100).toFixed(2)
-}
-
-
-//individual intervalIDS so altitude can keep going
-
-
-
-
-
-
-
-
-// random gauge values tied to gaugeMeter
-function randomGaugeFlux(input, variation) {
-    const min = input - variation
-    const max = input + variation
-    const rand = Math.floor(Math.random() * (max - min + 1)) + min
+/*
+Random Fluctuation Return
+*/
+function randomizeFlux(input, variation) {
+    const
+        min = input - variation,
+        max = input + variation,
+        rand = Math.floor(Math.random() * (max - min + 1)) + min
     return rand
 }
+
+
+
+
+
+// Constant Timer
+function startTimer() {
+    timeStart = Date.now() - timeElapsed
+    timeIntervalID = setInterval(updateTimer, 1000)
+
+    function updateTimer() { 
+        timeElapsed = Math.floor((Date.now() - timeStart) / 1000) 
+        console.log('timeElapsed:', timeElapsed);    
+    }
+
+    increaseVelocity()
+
+}
+
+
+
+
+
+
+
+startTimer()
+
+
+
+
+
+
+
+/*
+
+// Intertial gauges
+
+
+
 
 //Initialize Gauge Counter
 function gaugeEngage() {
     gaugeStart = Date.now() - gaugeElapsed
-    intervalID = setInterval(gaugeUpdate, intervalAmount)
-    let checkInterval = () => {
+    inertialID = setInterval(gaugeUpdate, inertialAmount)
+    // altitudeID = setInterval(gaugeUpdate, altitudeAmount)
+    
+    
+    function checkInertialInterval(){
         setInterval(() => {
-            if (allGaugeMeter >= 100) clearInterval(intervalID)
-        }, intervalAmount)
+            if (inertialGaugeMeter >= 100) clearInterval(inertialID)
+        }, inertialAmount)
     }
-    checkInterval()
+
+
+
+
+
+    checkInertialInterval()
+    checkAltitudeInterval()
 }
 
 // increase Gauges
 function gaugeUpdate() {
     gaugeElapsed = Date.now() - gaugeStart
-    allGaugeMeter = Math.floor((gaugeElapsed / intervalAmount) % 101)
+    inertialGaugeMeter = Math.floor((gaugeElapsed / inertialAmount) % 101)
+
 
     //inertialGauges
-    inertialVelocity = randomGaugeFlux(allGaugeMeter, 1)
+    inertialVelocity = randomGaugeFlux(inertialGaugeMeter, 1)
     inertialVelocitySlider.style.width = `${inertialVelocity}%`
     inertialVelocityText.textContent = `${UpdateInertialVelocityDisplay(inertialVelocity)} km/s`
 
 
-    //altitude
-    altitude = UpdateAltitudeDisplay(allGaugeMeter)
-    altitudeSlider.style.width = `${altitude}%`
-    altitudeText.textContent = `${altitude} km`
-
-    console.log("gaugeMeter:", allGaugeMeter, "velocity:", inertialVelocity)
-}
-
-gaugeEngage()
-
-
-
+*/
