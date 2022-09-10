a77f2d6b29bf1b4644916f976af47ce10715b6e1
KB <kj.br@protonmail.com>
2022-09-10 00:52:39 -0500

    refactor code

--- src/script.js
+++ src/script.js
@@ -22,40 +22,30 @@ Velocity
 let
     velocity = 0,
     velocityStart = 0,
-    velocityMeter = 0,
     velocityIntervalID
 
 
 function increaseVelocity() {
     velocityStart = Date.now() - timeElapsed
     velocityIntervalID = setInterval(updateVelocity, 100)
-    
 
-  
     function updateVelocity() {
-        velocity = Date.now() - velocityStart
-        velocityMeter = Math.floor((velocity/100))
-        
-       
-      
+        velocity = Math.floor(((Date.now() - velocityStart) / 500))
 
-        let variableVelocity = randomizeFlux(velocityMeter, 1)
+        let variableVelocity = randomizeFlux(velocity, 1)
         velocitySlider.style.width = variableVelocity + '%'
         velocityText.textContent = `${capVelocityText(variableVelocity)} km/s`
-        console.log({variableVelocity});
+        console.log({velocity});
+
+        if (variableVelocity >= 100) clearInterval(velocityIntervalID)
     }
 
-    function capVelocityText(velocity){
+    function capVelocityText(velocity) {
         let velocityDigits = velocity / 10
         return (velocityDigits < 10 ? velocityDigits : 10)
     }
 
-
-
 }
-
-
-
 
 
 function UpdateInertialVelocityDisplay(velocity) {
@@ -78,15 +68,13 @@ function randomizeFlux(input, variation) {
 
 
 
-
-
 // Constant Timer
 function startTimer() {
     timeStart = Date.now() - timeElapsed
     timeIntervalID = setInterval(updateTimer, 1000)
 
-    function updateTimer() { 
-        timeElapsed = Math.floor((Date.now() - timeStart) / 1000)   
+    function updateTimer() {
+        timeElapsed = Math.floor((Date.now() - timeStart) / 1000)
     }
 
     increaseVelocity()
@@ -103,48 +91,3 @@ startTimer()
 
 
 
-
-
-
-
-/*
-
-// Intertial gauges
-
-
-
-
-//Initialize Gauge Counter
-function gaugeEngage() {
-    gaugeStart = Date.now() - gaugeElapsed
-    inertialID = setInterval(gaugeUpdate, inertialAmount)
-    // altitudeID = setInterval(gaugeUpdate, altitudeAmount)
-    
-    
-    function checkInertialInterval(){
-        setInterval(() => {
-            if (inertialGaugeMeter >= 100) clearInterval(inertialID)
-        }, inertialAmount)
-    }
-
-
-
-
-
-    checkInertialInterval()
-    checkAltitudeInterval()
-}
-
-// increase Gauges
-function gaugeUpdate() {
-    gaugeElapsed = Date.now() - gaugeStart
-    inertialGaugeMeter = Math.floor((gaugeElapsed / inertialAmount) % 101)
-
-
-    //inertialGauges
-    inertialVelocity = randomGaugeFlux(inertialGaugeMeter, 1)
-    inertialVelocitySlider.style.width = `${inertialVelocity}%`
-    inertialVelocityText.textContent = `${UpdateInertialVelocityDisplay(inertialVelocity)} km/s`
-
-
-*/
